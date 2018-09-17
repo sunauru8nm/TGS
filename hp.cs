828723fc637e6dd6c617af64dd6e8b127b63e4f4
nojima <17d3170020nm@edu.tech.ac.jp>
2018-09-17 15:28:01 +0900

    no message

---
+++ hp.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class hp : MonoBehaviour
+{
+    public GameObject PartyDamage;
+    public GameObject EnemyDamage;
+
+    public Text PD;
+    public Text ED;
+
+    Slider _enemyslider;
+    Slider _partyslider;
+    // Use this for initialization
+    void Start()
+    {
+
+        PartyDamage.SetActive(false);
+        EnemyDamage.SetActive(false);
+        //スライダーを取得
+        _enemyslider = GameObject.Find("enemySlider").GetComponent<Slider>();
+
+        _partyslider = GameObject.Find("partySlider").GetComponent<Slider>();
+    }
+    //HPの値
+    public float partyhp = 200;
+    public float enemyhp = 800;
+    //HPを減らす
+    // Update is called once per frame
+    void Update()
+    {
+
+        //HPゲージに値を設定
+        _enemyslider.value = enemyhp;
+
+        _partyslider.value = partyhp;
+        if (partyhp <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+        if (enemyhp <= 0)
+        {
+            if(BattleManager.Instance.nowBattleScene == 2)
+            {
+                SceneManager.LoadScene("Win3");
+            }else if(BattleManager.Instance.nowBattleScene == 1){
+
+                SceneManager.LoadScene("Win2");
+            }
+            else
+            {
+
+                SceneManager.LoadScene("Win");
+            }
+        }
+    }
+    public void DownPartyHp(bool i, int j)
+    {
+        //防御成功：味方のHPを敵の攻撃力の20％分減らす
+        if (i == true)
+        {
+            float k = j * 0.2f;
+            partyhp -= k;
+            EnemyDamage.SetActive(true);
+            string X = k.ToString();
+            ED.text = X;
+            Invoke("DelayEnemyDamage", 0.5f);
+        }
+        else
+        {
+            //防御失敗：味方のHPを敵の攻撃力分減らす
+            partyhp -= j;
+            EnemyDamage.SetActive(true);
+            string Y = j.ToString();
+            ED.text = Y;
+            Invoke("DelayEnemyDamage", 0.5f);
+        }
+
+    }
+    public void DownEnemyHp(int i)
+    {
+       
+        enemyhp -= i;
+        PartyDamage.SetActive(true);
+        string Z = i.ToString();
+        PD.text = Z;
+
+        Invoke("DelayPartyDamage", 0.3f);
+     
+    }
+
+    void DelayPartyDamage() {
+        PartyDamage.SetActive(false);
+    }
+
+    void DelayEnemyDamage() {
+        EnemyDamage.SetActive(false);
+    }
+}
---
+++ menu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+
+public class menu : MonoBehaviour {
+
+    bool stop;
+    public GameObject menuScreen;
+
+	[SerializeField]GameObject touch;
+	TouchHantei touchHantei;
+    //Time.timescaleはUpdateは動いてFixedUpdateは動かない。
+    //Updateで動いているものをFixedUpdateに変えてもらう
+    public void MenuButton() {
+        //timescaleが1のときにクリックしたら0にしてstopをtrueにする
+        //timescaleが0のときにクリックしたら1にしてstopをfalseにする
+        if (Time.timeScale == 1){
+                Time.timeScale = 0;
+            stop = true;
+            }else if (Time.timeScale == 0){
+                Time.timeScale = 1;
+            stop = false;
+            }
+    }
+    private void Start(){
+		touchHantei = touch.GetComponent<TouchHantei>();
+        Time.timeScale = 1;
+        stop = false;
+        menuScreen.SetActive(false);
+    }
+
+    public void MenuScreen() {
+        //stopがtrueならmenuScreenが表示される
+        //stopがfalseならmenuScreenが非表示になる
+        if (stop == true){
+            menuScreen.SetActive(true);
+			touchHantei.toucjFlag = false;
+        }else if (stop == false) {
+            menuScreen.SetActive(false);
+			touchHantei.toucjFlag = true;
+        }
+    }
+
+    public void RetrunStart()
+    {
+
+        Time.timeScale = 1;
+        SceneManager.LoadScene("start");
+
+    }
+
+
+}
